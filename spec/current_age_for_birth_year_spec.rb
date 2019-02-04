11d22336004c8ce9380f276edc2cc8f09c6bbe37
Will Saffran <willh.saffran@gmail.com>
2019-02-04 21:48:52 +0000

    Automatically backed up by Learn

--- spec/current_age_for_birth_year_spec.rb
+++ spec/current_age_for_birth_year_spec.rb
@@ -6,8 +6,8 @@ describe "current_age_for_birth_year method" do
     birth_year = 1984
     answer = current_year - birth_year
     
-    age_of_person = current_age_for_birth_year(1984)
+    age_of_person = current_age_for_birth_year(birth_year)
 
-    expect(age_of_person).to eq(19)
+    expect(age_of_person).to eq(answer)
   end
 end
